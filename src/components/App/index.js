af765f684f00eff7ab63b64e9d3da2643785f7f0
Donovan Roubos <hello@donovanroubos.nl>
2020-02-06 23:06:24 +0100

    Remove unnecessary files

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,17 +9,6 @@ function App() {
     <div className="app">
       <header className="app-header">
         <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   )
